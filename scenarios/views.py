875bbf8da15d9ba799b2db7f4f0eb798faed67f0
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-02-17 09:13:30 -0800

    Add views

--- scenarios/views.py
+++ scenarios/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Assumptions
 
 
 def index(request):
-    return HttpResponse("Welcome to the Solrates Scenarios Demo.")
+    return HttpResponse("Welcome to the SolRates Scenarios Demo.")
+
+def detail(request, assumptions_id):
+    a = Assumptions.objects.get(pk=assumptions_id)
+    return HttpResponse("You're looking at scenario %s." % a)
+
+def results(request, assumptions_id):
+    a = Assumptions.objects.get(pk=assumptions_id)
+    response = "You're looking at the results of scenario %s."
+    return HttpResponse(response % a)
